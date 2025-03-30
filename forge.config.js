547af2502acdce17305f392ae7a0b64416e2d845
ZuluCharlie <casull88@gmail.com>
2025-03-29 20:34:34 -0500

    Attempting build workflow

--- forge.config.js
+++ forge.config.js
@@ -1,37 +1,57 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require("path");
+
 module.exports = {
   packagerConfig: {
     asar: true,
     osxSign: false, // Disable code signing
     osxNotarize: false, // Disable notarization
-    icon: './public/favicon',
-    extraResource: [
-      './public'
-    ]
+    icon: path.join(__dirname, 'public/favicon.ico'),
+    extraResource: ['./src/assets'],
+    name: 'zulu-wheel',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      platforms: ['win32'],
+      config: {
+        loadingGif: path.join(__dirname, 'public/ZSpin.gif'),
+        iconUrl: path.join(__dirname, 'public/favicon.ico'),
+        setupIcon: path.join(__dirname, 'public/favicon.ico'),
+        noMsi: true,
+        setupExe: 'zulu-wheel-setup-beta.exe'
+      },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'linux', 'win32']
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      platforms: ['linux'],
+      config: {
+        name: 'Zulu Wheel',
+        icon: 'public/favicon.ico',
+        maintainer: 'ZuluCharlie'
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      platforms: ['linux'],
+      config: {
+        name: 'Zulu Wheel',
+        icon: 'public/favicon.ico',
+      },
     },
     {
       name: '@electron-forge/maker-dmg',
+      platforms: ['darwin'],
       config: {
         format: 'ULFO',
         name: 'Zulu Wheel',
-        icon: './public/favicon.ico',
+        icon: 'public/favicon.ico',
         contents: options => {
           return [
             { x: 448, y: 344, type: 'link', path: '/Applications' },
@@ -39,12 +59,38 @@ module.exports = {
           ];
         }
       }
-    }
+    },
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
+      config: {
+        icon: path.join(__dirname, 'public/favicon.ico'),
+      },
     },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
   ],
-}; 
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'ZuluCharlie',
+          name: 'zulu-wheel'
+        },
+        prerelease: true,
+        draft: true
+      }
+    }
+  ]
+};
